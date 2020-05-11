f66c452e3ae378d7b37556f7445ff5f58182eada
poode <eng.abdul.azeem@gmail.com>
2020-05-11 15:53:16 +0200

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/.well-known/acme-challenge/:hash', (req, res , next) => {
+  console.log(req.params.hash);
+  res.sendFile(path.resolve(`./${req.params.hash}`));
+});
+
+app.listen(3000, () => console.log('up to verify!'));
